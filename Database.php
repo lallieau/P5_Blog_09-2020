a46afba914ca140fa6973cd9cd6e36ef2c46352a
lallie <lallie.audry@gmail.com>
2020-07-08 16:09:21 +0200

    initial commit

---
+++ Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database
+{
+    const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+
+    public function getConnection()
+    {
+        try{
+            $connection = new PDO(self::DB_HOST,self::DB_USER,self::DB_PASS);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return'Connexion Ok';
+        }
+
+        catch(Exception $errorConnection)
+        {
+            die('Erreur de connection :'.$errorConnection->getMessage());
+        }
+    }
+}
