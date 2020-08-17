3e8f36bc97db7dec9282420da8c58e60713b44b2
lallie <lallie.audry@gmail.com>
2020-08-17 14:53:05 +0200

    layout admin page, comment part and user

--- templates/administration.php
+++ templates/administration.php
@@ -10,102 +10,51 @@
 <?= $this->session->show('no_validate_comment'); ?>
 
 <h2>Articles</h2>
-<a href="index.php?route=addArticle">Nouvel article</a>
 <?php
 foreach ($articles as $article)
 {
 ?>
-<table>
-    <tr>
-        <td class="title">Id</td>
-        <td><?= htmlspecialchars($article->getId());?></td>
-        <td class="title">Titre</td>
-        <td><a href="index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></td>
-        <td class="title">Chapô</td>
-        <td><?= substr(htmlspecialchars($article->getChapo()), 0, 150);?>...</td>
-        <td class="title">Contenu</td>
-        <td><?= substr(htmlspecialchars($article->getContent()), 0, 150);?>...</td>
-        <td class="title">Auteur</td>
-        <td><?= htmlspecialchars($article->getAuthor());?></td>
-        <td class="title">Date</td>
-        <?php
-        if (!$article->getEditAt())
-        {
-            ?>
-            <td>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></td>
-            <?php
-        }
-        else
-        {
-            ?>
-            <td>Modifié le : <?= htmlspecialchars($article->getEditAt());?></td>
-            <?php
-        }
+<div class="card_admin">
+    <h3><a href="index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h3>
+    <?php
+    if (!$article->getEditAt())
+    {
         ?>
-        <td class="title">Actions</td>
-        <td>
-            <a href="index.php?route=editArticle&articleId=<?= $article->getId(); ?>">Modifier</a>
-            <a href="index.php?route=deleteArticle&articleId=<?= $article->getId(); ?>">Supprimer</a>
-        </td>
-    </tr>
+        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
+        <?php
+    }
+    else
+    {
+        ?>
+        <p>Modifié le : <?= htmlspecialchars($article->getEditAt());?></p>
+        <?php
+    }
+    ?>
+    <p><?= substr(htmlspecialchars($article->getChapo()), 0, 150);?>...</p>
+    <p><?= substr(htmlspecialchars($article->getContent()), 0, 150);?>...</p>
+    <p><?=ucfirst(htmlspecialchars($article->getAuthor()));?></p>
 
-
-</table><br>
+    <a href="index.php?route=editArticle&articleId=<?= $article->getId(); ?>">Modifier</a>
+    <a href="index.php?route=deleteArticle&articleId=<?= $article->getId(); ?>">Supprimer</a>
+</div>
     <?php
 }
 ?>
+<div class="add_article">
+    <a href="index.php?route=addArticle">Nouvel article</a>
+</div>
 
-<div class="accordion" id="accordionExample">
-    <div class="card">
-        <div class="card-header" id="headingOne">
-            <h3>
-                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <?= htmlspecialchars($article->getTitle());?><br> <span>&darr;</span>
-                </button>
-            </h3>
-        </div>
+<h2>Commentaires</h2>
 
-        <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
             <?php
             foreach($comments as $comment)
             {
-                if($comment->isValidation())
-                {
-                    ?>
-                    <div class="card-body">
-                        <h4><?=htmlspecialchars($comment->getPseudo());?></h4>
-                        <p><?=htmlspecialchars($comment->getContent());?></p>
-                        <h5>Posté le <?=htmlspecialchars($comment->getCreatedAt());?></h5>
-
-                    </div>
-                    <?php
-                }
-            }
             ?>
-            <h3>&Eacute;crire un commentaire</h3>
-            <?php include('form_comment.php'); ?>
-        </div>
+<div class="card_admin">
+                <h3><?=htmlspecialchars($comment->getPseudo());?></h3>
+                <p><?=htmlspecialchars($comment->getContent());?></p>
+                <p>Posté le <?=htmlspecialchars($comment->getCreatedAt());?></p>
 
-    </div>
-</div>
-<h2>Commentaires</h2>
-<table>
-    <tr>
-        <td>Id</td>
-        <td>Pseudo</td>
-        <td>Message</td>
-        <td>Date</td>
-    </tr>
-    <?php
-    foreach ($comments as $comment)
-    {
-        ?>
-        <tr>
-            <td><?= htmlspecialchars($comment->getId());?></td>
-            <td><?= htmlspecialchars($comment->getPseudo());?></td>
-            <td><?= substr(htmlspecialchars($comment->getContent()), 0, 150);?></td>
-            <td>Créé le : <?= htmlspecialchars($comment->getCreatedAt());?></td>
-            <td>
                 <?php
                 if($comment->isValidation())
                 {
@@ -121,47 +70,40 @@ foreach ($articles as $article)
                 }
                 ?>
                 <p><a href="index.php?route=deleteComment&commentId=<?= $comment->getId(); ?>">Supprimer le commentaire</a></p>
-                <br>
+
+</div>
                 <?php
-                }
-                ?>
-                 </td>
-        </tr>
-</table>
+            }
+            ?>
+
+
+
+
 
 <h2>Utilisateurs</h2>
-<table>
-    <tr>
-        <td>Id</td>
-        <td>Pseudo</td>
-        <td>Date</td>
-        <td>Rôle</td>
-        <td>Actions</td>
-    </tr>
-    <?php
-    foreach ($users as $user)
-    {
-        ?>
-        <tr>
-            <td><?= htmlspecialchars($user->getId());?></td>
-            <td><?= htmlspecialchars($user->getPseudo());?></td>
-            <td>Créé le : <?= htmlspecialchars($user->getCreatedAt());?></td>
-            <td><?= htmlspecialchars($user->getRole());?></td>
-            <td>
-                <?php
-                if($user->getRole() != 'admin') {
-                    ?>
-                    <a href="index.php?route=deleteUser&userId=<?= $user->getId(); ?>">Supprimer</a>
-                <?php }
-                else {
-                    ?>
-                    Suppression impossible
-                    <?php
-                }
-                ?>
-            </td>
-        </tr>
+
+<?php
+foreach ($users as $user)
+{
+?>
+<div class="card_admin">
+    <p>Pseudo : <?= htmlspecialchars($user->getPseudo());?></p>
+    <p>Date : <?= htmlspecialchars($user->getCreatedAt());?></p>
+    <p>Rôle : <?= htmlspecialchars($user->getRole());?></p>
+    <p>Actions :
         <?php
-    }
-    ?>
-</table>
+        if($user->getRole() != 'admin') {
+            ?>
+            <a href="index.php?route=deleteUser&userId=<?= $user->getId(); ?>">Supprimer</a>
+        <?php }
+        else {
+            ?>
+            Suppression impossible
+            <?php
+        }
+        ?>
+    </p>
+</div>
+<?php
+}
+?>
