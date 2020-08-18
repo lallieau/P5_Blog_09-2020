356d4f35cb8faede1927dad5a4e8e328be38b645
lallie <lallie.audry@gmail.com>
2020-08-18 17:52:02 +0200

    resolve navbar links, delete script, modify css (remains to be finished)

--- templates/navbar_mobil.php
+++ templates/navbar_mobil.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8"/>
-    <title>header</title>
-</head>
-<body>
+<?php $this->title="Navbar mobile"; ?>
 <nav>
-    <a class="active" href="index.php">
+    <a href="index.php" class="link_home">
         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
             <path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
@@ -16,7 +10,7 @@
     </span>
     </a>
 
-    <a href="index.php?route=articlesPage">
+    <a href="index.php?route=articlesPage" class="link_articles">
         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-journal-richtext" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1H2a2 2 0 0 1 2-2z"/>
             <path d="M2 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H2z"/>
@@ -27,83 +21,22 @@
     </span>
     </a>
 
-    <a href="index.php?route=contactPage">
+    <a href="index.php?route=contactPage" class="link_contact">
         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chat-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
             <path fill-rule="evenodd" d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
         </svg>
-        <span>
+        <span >
       Contact
     </span>
     </a>
 
-    <a href="index.php?route=profile">
+    <a href="index.php?route=profile" class="link_profil">
         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
         </svg>
-        <span>
+        <span >
       Profil
     </span>
     </a>
 </nav>
-</body>
-
-</html>
-
-<script>
-    // array describing the options of the navigation elements
-    // specifying the lower case option and the matching color
-    const navigationOptions = [
-        {
-            name: 'home',
-            color: '#5B37B7'
-        },
-        {
-            name: 'articles',
-            color: '#C9379D'
-        },
-        {
-            name: 'contact',
-            color: '#E6A919'
-        },
-        {
-            name: 'profil',
-            color: '#1892A6'
-        }
-    ];
-
-    // target all anchor link elements
-    const links = document.querySelectorAll('nav a');
-
-    // function called in response to a click event on the anchor link
-    function handleClick(e) {
-        // prevent the default behavior, but most importantly remove the class of .active from those elements with it
-        e.preventDefault();
-        links.forEach(link => {
-            if (link.classList.contains('active')) {
-                link.classList.remove('active');
-            }
-        });
-
-        // retrieve the option described the link element
-        const name = this.textContent.trim().toLowerCase();
-        // find in the array the object with the matching name
-        // store a reference to its color
-        const { color } = navigationOptions.find(item => item.name === name);
-
-        // retrieve the custom property for the --hover-c property, to make it so that the properties are updated only when necessary
-        const style = window.getComputedStyle(this);
-        const hoverColor = style.getPropertyValue('--hover-c');
-        // if the two don't match, update the custom property to show the hue with the text and the semi transparent background
-        if (color !== hoverColor) {
-            this.style.setProperty('--hover-bg', `${color}20`);
-            this.style.setProperty('--hover-c', color);
-        }
-
-        // apply the class of active to animate the svg an show the span element
-        this.classList.add('active');
-    }
-
-    // listen for a click event on each and every anchor link
-    links.forEach(link => link.addEventListener('click', handleClick));
-</script>
--- templates/profile.php
+++ templates/profile.php
@@ -24,21 +24,22 @@
     <div class="card_admin">
     <a href="index.php?route=logout">Déconnexion</a>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Mot de passe</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php include('update_password.php'); ?>
+                </div>
+            </div>
+        </div>
 </div>
 
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">Mot de passe</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <?php include('update_password.php'); ?>
-            </div>
-        </div>
-    </div>
