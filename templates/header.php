ed1066a6a598038285fa3000afdee7758ae29405
lallie <lallie.audry@gmail.com>
2020-08-20 15:45:45 +0200

    add link cv in navbar header

--- templates/header.php
+++ templates/header.php
@@ -39,6 +39,9 @@
             <?php
             }
             ?>
+            <li class="nav-item">
+                <a class="nav-link" href="pdf/cv-lallie-audry.pdf">curriculum vitae</a>
+            </li>
 
         </ul>
     </div>
