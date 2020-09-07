c889e1d3deeb852b958990b31487ae25955aef94
lallie <lallie.audry@gmail.com>
2020-09-07 10:07:06 +0200

    clean files and folders, clean up sql queries for database creation

--- sql/db.sql
+++ sql/db.sql
@@ -1,46 +1,67 @@
 CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE `article` (
+/* Article Table */
 
+CREATE TABLE `article` (
 `id` int(11)NOT NULL AUTO_INCREMENT,
 `title` varchar(100)NOT NULL,
 `content` text NOT NULL,
-`author` varchar(100) NOT NULL,
 `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`chapo` text NOT NULL,
+`EditAt` datetime NULL,
+`img` text NOT NULL,
+`bg` text NOT NULL,
+`user_id` int(11) NOT NULL,
 
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `article` (`title`, `content`, `author`)VALUES
-
-('Premier article','Mon blog est en construction, patience','Lallie'),
-
-('Deuxième article','Je continue à ajouter du contenu sur mon blog','Lallie'),
-
-('Troisième article','Mon blog est génial !','Lallie')
-
+/* Comment Table */
 
 CREATE TABLE `comment`(
 `id`int(11) NOT NULL AUTO_INCREMENT,
 `pseudo` varchar(100) NOT NULL,
 `content` text NOT NULL,
 `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`validation` tinyint(1) NOT NULL,
 `article_id` int(11) NOT NULL,
 
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+/* User Table */
+
+CREATE TABLE `user`(
+`id`int(11) NOT NULL AUTO_INCREMENT,
+`pseudo` varchar(100) NOT NULL,
+`password` varchar(60) NOT NULL,
+`createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`sexe`int(11) NOT NULL,
+`role_id`int(11) NOT NULL,
+
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/* Role Table */
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+
+PRIMARY KEY (`id`);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+/* Foreign Key */
+
 ALTER TABLE `comment`
 ADD CONSTRAINT `fk_article_id` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`);
 
-INSERT INTO `comment` (`pseudo`,`content`,`article_id`)VALUES
-('Jean', 'Génial, hâte de voir ce que ça donne !',1),
-('Nina', 'Trop cool ! depuis le temps',1),
-('Rodrigo', 'Great ! ',1),
-('Hélène', 'je suis heureuse de découvrir un super site ! Continuez comme ça ',2),
-('Moussa', 'Un peu déçu par le contenu pour le moment...',2),
-('Barbara', 'pressée de voir la suite',2),
-('Guillaume', 'Je viens ici pour troller !',3),
-('Aurore', 'Enfin un blog tranquille, où on ne nous casse pas les pieds !',3),
-('Jordane', 'Je suis vendéen ! Amateur de mojettes !',3);
+ALTER TABLE `article`
+ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
+ALTER TABLE `user`
+ADD CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
